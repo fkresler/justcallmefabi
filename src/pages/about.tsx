ad065b5fc9432359093cd1f4c3833a9b65a12d22
Fabian Kresler <fabi.kresler@googlemail.com>
2021-09-19 00:01:38 +0200

    feat: remove unused pages

--- src/pages/about.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { Layout } from '../components/Layout';
-
-const AboutPage: React.FC = () => <Layout>Itsa me, Fabian Kresler!</Layout>;
-
-export default AboutPage;
--- src/pages/contact.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { Layout } from '../components/Layout';
-
-const ContactPage: React.FC = () => <Layout>Contact me pls!</Layout>;
-
-export default ContactPage;
