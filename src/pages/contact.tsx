30d70f8990572dd08c659849bda43b4c6d581768
Fabian Kresler <fabi.kresler@googlemail.com>
2022-04-01 13:57:40 +0200

    fix: add missing contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Layout } from '../components/Layout';
+
+const ContactPage: React.FC = () => (
+  <Layout>
+    <h1>Here you will find info on how to contact me!</h1>
+  </Layout>
+);
+
+export default ContactPage;
